12904abb50e08b8a61f82253a7d0b2c28b5e7211
NamOfNames <ckn0203@gmail.com>
2023-09-10 10:05:13 +0700

    commit

--- node.js
+++ node.js
@@ -1,16 +1,29 @@
-const url = 'https://moviesdatabase.p.rapidapi.com/titles/series/%7BseriesId%7D';
+const DEFAULT_VALUE = "--";
+const searchInput = document.getElementById("search-input");
+const movieImage = document.querySelector(".movie-image");
+
+const url = "https://imdb8.p.rapidapi.com/title/find?q=spiderman";
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '83fcb7fadfmshe81b9904851bb6dp18240bjsne87fa9e95105',
-		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "83fcb7fadfmshe81b9904851bb6dp18240bjsne87fa9e95105",
+    "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
+  },
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+searchInput.addEventListener("change", (event) => {
+  fetch(
+    `https://imdb8.p.rapidapi.com/title/find?q=${event.target.value}`,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.results);
+      types = [];
+      length = data.results.length;
+      for (let i = 0; i < length; i++) {
+        types.push(data.results[0].image.url);
+      }
+	  movieImage.src = (types) || DEFAULT_VALUE
+    });
+});
