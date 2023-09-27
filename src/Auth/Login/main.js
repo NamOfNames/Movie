7db7b07d807e731562666c8aededa01b97b35efd
NamOfNames <ckn0203@gmail.com>
2023-09-27 15:14:56 +0700

    commit

--- src/Auth/Login/main.js
+++ src/Auth/Login/main.js
@@ -11,19 +11,19 @@ function validation() {
   if (valueEmail == "") {
     // check giá trị nhập vào của email có hay không
     errorEmailNoti.innerHTML = "Chưa Nhập Vào Email"; // thay đổi text của error
-    errorEmailNoti.style = "color: red; display: block"; //thay đổi style của error
+    errorEmailNoti.style = "color: yellow; display: block"; //thay đổi style của error
   } else if (!valueEmail.includes("@")) {
     // check email có dấu @ hay không
     errorEmailNoti.innerHTML = "Email không đúng định dạng";
-    errorEmailNoti.style = "color: red; display: block";
+    errorEmailNoti.style = "color: yellow; display: block";
   } else if (!valueEmail.includes(".")) {
     errorEmailNoti.innerHTML = "Email không đúng định dạng";
-    errorEmailNoti.style = "color: red; display: block";
+    errorEmailNoti.style = "color: yellow; display: block";
   } else if (valueEmail.includes("@") & valueEmail.includes(".")) {
     errorEmailNoti.style = "display:none";
   } else if (valuePassword == "") {
     errorPasswordNoti.innerHTML = "Chưa Nhập Vào Password";
-    errorPasswordNoti.style = "color: red; display: block";
+    errorPasswordNoti.style = "color: yellow; display: block";
   } else {
     let linkHome = document.getElementById("linkHome");
     linkHome.href = "../index.html";
--- src/Auth/Register/main.js
+++ src/Auth/Register/main.js
@@ -8,15 +8,26 @@ let errorEmailNoti = document.getElementById("error_email_noti");
 let errorPasswordNoti = document.getElementById("error_password_noti");
 let ConfirmPasswordNoti = document.getElementById("confirm_password_noti");
 
+Info_storage = [];
+
 function validation() {
   let valueName = inputName.value;
   let valueEmail = inputEmail.value;
   let valuePassword = inputPassword.value;
   let valueConfirmPassword = inputConfirmPassword.value;
+  var atposition = valueEmail.indexOf("@");
+  var dotposition = valueEmail.lastIndexOf(".");
+  if (
+    atposition < 1 ||
+    dotposition < atposition + 2 ||
+    dotposition + 2 >= valueEmail.length
+  ) {
+    errorEmailNoti.style = "color: yellow; display: block";
+  }
 
   if (valueName == "") {
     errorNameNoti.innerHTML = "Chưa Nhập Vào Họ và tên";
-    errorNameNoti.style = "color: red; display: block";
+    errorNameNoti.style = "color: yellow; display: block";
   } else {
     errorNameNoti.style = "display: none";
   }
@@ -24,35 +35,40 @@ function validation() {
   if (valueEmail == "") {
     // check giá trị nhập vào của email có hay không
     errorEmailNoti.innerHTML = "Chưa Nhập Vào Email"; // thay đổi text của error
-    errorEmailNoti.style = "color: red; display: block"; //thay đổi style của error
+    errorEmailNoti.style = "color: yellow; display: block"; //thay đổi style của error
   } else if (!valueEmail.includes("@")) {
     // check email có dấu @ hay không
     errorEmailNoti.innerHTML = "Email không đúng định dạng";
-    errorEmailNoti.style = "color: red; display: block";
+    errorEmailNoti.style = "color: yellow; display: block";
   } else if (!valueEmail.includes(".")) {
     errorEmailNoti.innerHTML = "Email không đúng định dạng";
-    errorEmailNoti.style = "color: red; display: block";
+    errorEmailNoti.style = "color: yellow; display: block";
   } else {
     errorEmailNoti.style = "display: none"; // nếu những trường hợp trên đã đúng thì text error sẽ ẩn đi
   }
 
   if (valuePassword == "") {
     errorPasswordNoti.innerHTML = "Chưa Nhập Vào Password";
-    errorPasswordNoti.style = "color: red; display: block";
+    errorPasswordNoti.style = "color: yellow; display: block";
   } else {
     errorPasswordNoti.style = "display: none";
   }
 
-  if (valueConfirmPassword == "") {
+  if (valueConfirmPassword == "" || valuePassword != valueConfirmPassword) {
     ConfirmPasswordNoti.innerHTML = "Mật khẩu không giống nhau";
-    ConfirmPasswordNoti.style = "color: red; display: block";
-  } else if (valuePassword != valueConfirmPassword) {
-    ConfirmPasswordNoti.innerHTML = "Mật khẩu không giống nhau";
-    ConfirmPasswordNoti.style = "color: red; display: block";
-  } else if (valuePassword == valueConfirmPassword) {
-    ConfirmPasswordNoti.innerHTML = "Mật khẩu giống nhau";
-    ConfirmPasswordNoti.style = "color: green; display: block";
+    ConfirmPasswordNoti.style = "color: yellow; display: block";
   } else {
     ConfirmPasswordNoti.style = "display: none";
+  }
+
+  if (
+    (valueName != "") &
+    (valueEmail != "") &
+    (valuePassword == valueConfirmPassword)
+  ) {
+    console.log("YES SIR");
+    Info_storage.push(valueEmail, valuePassword);
+    console.log(Info_storage);
+    localStorage.setItem("Email", Info_storage);
   }
 }
